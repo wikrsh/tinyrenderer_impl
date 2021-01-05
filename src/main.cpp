b00a5f14be855d4f9e4edcb6c46c143713bd1efc
Wataru Ikarashi <wikrsh@gmail.com>
2021-01-05 23:57:42 +0900

    First attempt

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include "tgaimage.h"
 
-const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor white = TGAColor(255, 255, 255, 255);
+
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+  for (float t = 0.0; t < 1.0; t += 0.01) {
+    int x = x0 + (x1 - x0) * t;
+    int y = y0 + (y1 - y0) * t;
+    image.set(x, y, color);
+  }
+}
 
 int main() {
-  TGAImage image(100, 100, TGAImage::RGB);
-  image.set(52, 41, red);
-  image.flip_vertically();
+  TGAImage image(400, 400, TGAImage::RGB);
+
+  line(80, 90, 300, 200, image, white);
+
   image.write_tga_file("output.tga");
 
   return 0;
