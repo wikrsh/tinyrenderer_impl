f70f8541b12a60672f2416a9b579387de1e7cf10
Wataru Ikarashi <wikrsh@gmail.com>
2021-01-06 23:16:43 +0900

    Wireframe rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "tgaimage.h"
+#include "geometry.h"
+#include "Model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -38,12 +40,27 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 }
 
 int main() {
-  TGAImage image(400, 400, TGAImage::RGB);
+  const int width = 400;
+  const int height = 400;
+  TGAImage image(width, height, TGAImage::RGB);
+  Model model("../src/obj/african_head.obj");
 
-  line(80, 90, 300, 200, image, white);
-  line(90, 80, 200, 300, image, red);
-  line(300, 200, 80, 90, image, red);
+  for (int i = 0; i < model.nfaces(); i++) {
+    auto face = model.face(i);
+    for (int j = 0; j < 3; j++) {
+      auto v0 = model.vert(face[j]);
+      auto v1 = model.vert(face[(j+1) % 3]);
 
+      int x0 = (v0.x + 1.0) * width / 2.0;
+      int y0 = (v0.y + 1.0) * height / 2.0;
+      int x1 = (v1.x + 1.0) * width / 2.0;
+      int y1 = (v1.y + 1.0) * height / 2.0;
+
+      line(x0, y0, x1, y1, image, white);
+    }
+  }
+
+  image.flip_vertically();
   image.write_tga_file("output.tga");
 
   return 0;
