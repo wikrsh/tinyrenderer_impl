f451503ef14356e3aa21241f4c834bed19e9a6f8
Wataru Ikarashi <wikrsh@gmail.com>
2021-01-07 00:02:01 +0900

    Refactor

---
+++ src/renderer.h
@@ -0,0 +1,8 @@
+#ifndef TINYRENDERER_IMPL_RENDERER_H
+#define TINYRENDERER_IMPL_RENDERER_H
+
+#include "tgaimage.h"
+
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color);
+
+#endif //TINYRENDERER_IMPL_RENDERER_H
